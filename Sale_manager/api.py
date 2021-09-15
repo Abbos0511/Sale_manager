ee912a5f80141ca55bb10f628f1bfc65117924eb
Baxtiyor98 <ergashevb111@gmail.com>
2021-09-15 07:24:21 +0500

    api chiqarildi
    api chiqarildi

--- Sale_manager/api.py
+++ Sale_manager/api.py
@@ -1,5 +1,14 @@
 from app.views import *
 from rest_framework import routers
 router = routers.DefaultRouter()
-
+router.register(r'category',CategoryView,basename='category')
+router.register(r'brand',BrandView,basename='brand')
+router.register(r'product',ProductView,basename='product')
+router.register(r'product-by-category',ProductByCategoryView,basename='product-by-category')
+router.register(r'stock',StockView,basename='stock')
+router.register(r'unit',UnitView,basename='unit')
+router.register(r'price-history',PriceHistoryView,basename='price-history')
+router.register(r'price-history-by-product',PriceHistoryByProductView,basename='price-history-by-product')
+router.register(r'supplier',SupplierView,basename='supplier')
+router.register(r'debit',DebitView,basename='debit')
 router.register(r'qiuck_panel', QuickPanelView, basename='quick_panel')
--- app/views.py
+++ app/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets,status
 from .serializer import *
-
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+class UnitView(viewsets.ModelViewSet):
+    queryset = Unit.objects.all()
+    serializer_class = UnitSerializer
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class ProductByCategoryView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class StockView(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+class PriceHistoryView(viewsets.ModelViewSet):
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+class PriceHistoryByProductView(viewsets.ModelViewSet):
+    queryset = PriceHistory.objects.all()
+    serializer_class = PriceHistorySerializer
+class SupplierView(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+class DebitView(viewsets.ModelViewSet):
+    queryset = Debit.objects.all()
+    serializer_class = DebitSerializer
 class QuickPanelView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = QuickPanelSerializer
